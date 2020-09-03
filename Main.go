f2a3c1f6019445a481647b049d4133b6d800ed18
Tomi <juhasz.tamas991@gmail.com>
2020-09-03 20:57:53 +0200

    feat: get and post todo

--- Main.go
+++ Main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/google/uuid"
 
 	"github.com/gorilla/mux"
 )
 
-var state []Todo
+var state map[uuid.UUID]Todo
 
 func main() {
 	r := mux.NewRouter()
 
 	//routes
 
-	r.HandleFunc("/", handleRoot).Methods("GET")
+	r.HandleFunc("/todos", handleRoot).Methods("GET")
 
 	r.HandleFunc("/todos", handleAddTodo).Methods("POST")
+
+	r.HandleFunc("/todos/{todoId}", handleGetTodo).Methods("GET")
 
 	r.HandleFunc("/todos/{todoId}", handleUpdateTodo).Methods("PUT")
 
@@ -31,20 +36,57 @@ func main() {
 
 //handleroot loads the root page.
 func handleRoot(w http.ResponseWriter, r *http.Request) {
+	if len(state) <= 0 {
+		state = make(map[uuid.UUID]Todo)
+	}
+
 	listOfJSON, _ := json.Marshal(state)
 
 	w.Write(listOfJSON)
 }
 
+func handleGetTodo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id, err := uuid.Parse(vars["todoId"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	todo, isPresent := state[id]
+	if !isPresent {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	writeResponse(todo, w)
+}
+
 //handleAddTodo adds a new todo.
 func handleAddTodo(w http.ResponseWriter, r *http.Request) {
+	if len(state) <= 0 {
+		state = make(map[uuid.UUID]Todo)
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(body))
 	var todo Todo
+	err = json.Unmarshal(body, &todo)
+	if err != nil {
+		fmt.Println(todo)
+		w.WriteHeader(400)
+		return
+	}
+	todo.ID, _ = uuid.NewRandom()
+	state[todo.ID] = todo
 
-	json.NewDecoder(r.Body).Decode(&todo)
-	state = append(state, todo)
-
-	todoJSON, _ := json.Marshal(todo)
-	w.Write(todoJSON)
+	writeResponse(todo, w)
 }
 
 //handleUpdateTodo updates a todo.
@@ -59,5 +101,12 @@ func handleDeleteTodo(w http.ResponseWriter, r *http.Request) {
 
 //Todo type
 type Todo struct {
-	Body string `json:"body"`
+	ID   uuid.UUID `json:"id"`
+	Body string    `json:"body"`
+}
+
+// helper function to write response
+func writeResponse(todo Todo, w http.ResponseWriter) {
+	todoJSON, _ := json.Marshal(todo)
+	w.Write(todoJSON)
 }
